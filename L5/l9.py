64552c7a45b671a4997600018069d6bbdd91a423
bobo <aflorei.paul@gmail.com>
2025-05-15 13:31:21 +0300

    L11

--- L5/l9.py
+++ L5/l9.py
@@ -6,8 +6,12 @@ import requests
 Q = queue.Queue(100)
 Q.put('https://www.robotstxt.org')
 
+
+
 while not Q.empty() or not Q.full():
     L = Q.get()
     if L:
         continue
-    
+    P = requests.get(L)
+    if P:
+        continue
